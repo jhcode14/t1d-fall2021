1910df4e4f7551857174eea5e5be244a386b1a9d
trisaleung <trisaleung1812@gmail.com>
2021-10-16 11:54:47 -0700

    created basic grid for Overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,18 +7,27 @@ class Overview extends React.Component {
     render() {
         return (
             <Container>
-                <Row>
-                    <Col>
+                <Row className="grid">
+                    <Col 
+                        id="minutes"
+                        className="boxes"
+                    >
                         <h2 className="number">15</h2>
                         <h3 className="stat">minutes</h3>
                         <p className="description">with user friendly interfaces</p>
                     </Col>
-                    <Col>
+                    <Col 
+                        id="cost"
+                        className="boxes"
+                    >
                         <h2 className="number">Free</h2>
                         <h3 className="stat">of cost</h3>
                         <p className="description">Open source code base</p>
                     </Col>
-                    <Col>
+                    <Col 
+                        id="research"
+                        className="boxes"
+                    >
                         <h2 className="number">Research</h2>
                         <h3 className="stat">by the Diabetes Design Initiative</h3>
                         <p className="description">Tested with PWD and healthcare providers</p>
