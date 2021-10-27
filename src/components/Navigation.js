db5dcc68f1ea4fc0c1b003f4aae74edb98562e7a
jhcode14 <jhsucode@gmail.com>
2021-10-27 13:34:17 -0700

    in progress tweak (make black button dissapear in phone size screen)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,34 +6,32 @@
 */
 
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import { NormalButton, BlackButton } from "./Button.style";
 import "../styles/nav.css";
 
-import logo from '../images/ddi_logo.png';
+import logo from "../images/ddi_logo.png";
 
 class Navigation extends React.Component {
-    render() {
-        return (
-            <Container id="nav_spacing">
-                <Row id="Ngrid">
-                    <Col id="Nlogo">
-                        <img
-                        src={logo}
-                        width="400"
-                        height="100"
-                        alt="T1D logo"
-                        />
-                    </Col>
-                    <Col>
-                        <NormalButton className='NButtonText'> Create Mine</NormalButton>
-                    </Col>
-                    <Col>
-                        <BlackButton className='NButtonText'> Feedback Form</BlackButton>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
-};
+  render() {
+    return (
+      <Container id="nav_spacing">
+        <Row id="Ngrid">
+          <Col id="Nlogo">
+            <img src={logo} className="navLogo" alt="T1D logo" />
+          </Col>
+          <Col>
+            <NormalButton className="NButtonText"> Create Mine</NormalButton>
+          </Col>
+          <Col>
+            <BlackButton className="BlackNButtonText">
+              {" "}
+              Feedback Form
+            </BlackButton>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 export default Navigation;
