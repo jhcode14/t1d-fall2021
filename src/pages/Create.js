e404e7e726bbce4106f8b18abc60ed8ee955c6e3
jhcode14 <jhcode@gmail.com>
2021-11-12 00:08:51 -0800

    started work on jsPDF

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,35 +1,72 @@
 import React from "react";
 import * as Survey from "survey-react";
+//import CreatePDF from "./CreateDoc.js";
 
-Survey
-    .StylesManager
-    .applyTheme("modern");
+
+import jsPDF from 'jspdf';
+function CreatePDF(InputList) {
+  // Default format
+  // 216mm x 279mm for Letter
+  var doc = new jsPDF({
+      orientation: 'p',
+      unit: 'mm',
+      format: 'letter',
+  });
+  doc.setFont("times", "normal");
+  doc.setFontSize(10);
+  doc.text('Alexise Berousbrerg D.O.B 4/6/1974', 210.71, 4.23, {align: 'right'})
+  doc.text('Last updated: 8/23/2021', 210.71, 9.51, {align: 'right'})
+  doc.save('T1D_CareDirective.pdf')
+}
+
+//Survey
+    //.StylesManager
+    //.applyTheme("modern");
 
 var surveyJSON = {
   title: "Type 1 Diabeties Care Directive Form Creation",
-  "showProgressBar": "top",
+  showProgressBar: "top",
   pages: [
     {
       name: "page1",
-      questions: [
-        {
+      elements: [
+       {
+        type: "paneldynamic",
+        name: "General Information",
+        templateElements: [
+         {
           type: "text",
-          isRequired: false,
-          name: "Name",
-          placeHolder: "e.g. Jonney Appleseed",
+          name: "name",
           title: "Full Name",
-        },
-        {
+          autoComplete: "name",
+          placeHolder: "eg. Johnny Appleseed"
+         },
+         {
           type: "text",
-          inputType: "date",
-          hasOther: true,
-          isRequired: false,
           name: "birthdate",
           title: "Date of Birth",
-          autoComplete: "bdate",
-        },
-      ],
-    },
+          inputType: "date",
+          autoComplete: "bdate"
+         }
+        ]
+       },
+       {
+        type: "paneldynamic",
+        name: "question4",
+        startWithNewLine: false,
+        templateElements: [
+         {
+          type: "text",
+          name: "question3"
+         },
+         {
+          type: "text",
+          name: "question2"
+         }
+        ]
+       }
+      ]
+     },
     {
       name: "page2",
       questions: [
@@ -84,6 +121,10 @@ function Create() {
         />
       </head>
       <Survey.Survey json={surveyJSON} onComplete={sendDataToServer} />
+
+      <div>
+        <button onClick={CreatePDF} type="primary">Download PDF</button>
+      </div>
     </React.Fragment>
   );
 }
