c977fde961948810fc1c3709108e9197cc0f8162
jhcode14 <jhsucode@gmail.com>
2022-01-03 03:11:30 -0800

    survey pg1 hardcode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "antd/dist/antd.css";
 import "./styles/fonts.css";
 
 import React from "react";
--- src/components/JSurveyDemo.jsx
+++ src/components/JSurveyDemo.jsx
@@ -1,12 +1,93 @@
 import React from "react";
 import { useState } from "react";
 import { Container } from "react-bootstrap";
-import { Button } from "antd";
+import { Button, Row, Col, Input, DatePicker, Radio } from "antd";
 import "../styles/survey_demo.css";
 
 // ToDo: disable next/previous button when page 1 or 7
 //       show different pane
 //       customize different pane
+
+const data = {};
+
+function Questionaries(key) {
+  const paneKey = key.number;
+  console.log(paneKey);
+  let display;
+  if (paneKey === 1) {
+    display = (
+      <Row gutter={8}>
+        <Col span={12}>
+          <div>Full Name</div>
+          <Input placeholder="eg. Johnny Appleseed" />
+          <div>Date of Birth</div>
+          <Row gutter={8}>
+            <Col span={6}>
+              <Input placeholder="Month" />
+            </Col>
+            <Col span={4}>
+              <Input placeholder="Day" />
+            </Col>
+            <Col span={4}>
+              <Input placeholder="Year" />
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12}>
+          <div>What is your current HbA1c?</div>
+          <Row>
+            <Col span={8}>
+              <Input placeholder="HbA1c" />
+            </Col>
+            <Col span={16}>
+              <Radio.Group defaultValue="a" buttonStyle="solid">
+                <Radio.Button value="a">mmol/mol</Radio.Button>
+                <Radio.Button value="b">Percentage(%)</Radio.Button>
+              </Radio.Group>
+            </Col>
+          </Row>
+          <div>Date Recorded</div>
+          <DatePicker />
+        </Col>
+      </Row>
+    );
+  } else if (paneKey === 2) {
+    display = <div>2</div>;
+  } else if (paneKey === 3) {
+    display = <div>3</div>;
+  } else if (paneKey === 4) {
+    display = <div>4</div>;
+  } else if (paneKey === 5) {
+    display = <div>5</div>;
+  } else if (paneKey === 6) {
+    display = <div>6</div>;
+  } else if (paneKey === 7) {
+    display = <div>7</div>;
+  } else {
+    display = <div>ERROR</div>;
+    console.error(`invalid activePane input ${paneKey}`);
+  }
+
+  return display;
+}
+
+function progressBar(key) {
+  const paneKey = key.number;
+  console.log(paneKey);
+  let display;
+  if (paneKey === 1) {
+  } else if (paneKey === 2) {
+  } else if (paneKey === 3) {
+  } else if (paneKey === 4) {
+  } else if (paneKey === 5) {
+  } else if (paneKey === 6) {
+  } else if (paneKey === 7) {
+  } else {
+    console.error(`invalid activePane input ${paneKey}`);
+  }
+
+  return display;
+}
 
 function JSurveyDemo() {
   const [activePane, setActivePane] = useState(1);
@@ -34,6 +115,8 @@ function JSurveyDemo() {
           Any information in this section will be listed on a second page, so
           your document will be a total of two pages.
         </p>
+        <Questionaries number={activePane} />
+        <div>`currently at {activePane}`</div>
         <div>
           <Button onClick={onClickPrevious}>Previous</Button>
           <Button type="primary" onClick={onClickNext}>
