85bc22ee7e8adff9b7abdff88f58f4bd6ad7c1f7
Eyvonne Hu <eyvonnehu@gmail.com>
2022-02-17 16:28:58 -0800

    added creation gif

--- src/components/CreationGIF.js
+++ src/components/CreationGIF.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import '../styles/creationgif.css'
+import '../styles/creationgif.css';
+import example from '../images/example.gif';
 
 class CreationGIF extends React.Component {
     render() {
         return(
             <div id="gif_box">
-                temporary placeholder for gif
+                <img src={example} id="gif_img"/>
             </div>
         );
     }
