432cca54a38d309f6c28a5e768404e2072fbea0a
jhcode14 <jhsucode@gmail.com>
2022-03-22 23:22:26 -0700

    easy fix

--- src/components/Example.js
+++ src/components/Example.js
@@ -3,44 +3,16 @@ import { NormalButton } from "./Button.style";
 
 import '../styles/example.css';
 
-import doctor from '../images/doctor_standing.png';
-import patient from '../images/person_standing.png';
-import speech1 from '../images/speech1.png';
-import speech2 from '../images/speech2.png'
+import convo from '../images/convo.png';
 import { Link } from "react-router-dom";
 
 class Example extends React.Component {
     render() {
         return (
             <div>
-                <div id="left_image">
-                    <div id="doctor">
-                        <img src={doctor} />
-                    </div>
-                    <div id="first_bubble">
-                        <img src={speech1} />
-                        <div
-                            id="first_speech" className="speech_bubble"
-                        >
-                            What should I know about your Type 1 diabetes?
-                        </div>
-                    </div>
+                <div id="doctor">
+                    <img src={convo} alt="conversation" />
                 </div>
-                <div id="right_image">
-                    <div id="patient">
-                        <img src={patient} />
-                    </div>
-                    <div id="second_bubble">
-                        <img src={speech2} />
-                        <div
-                            id="second_speech"
-                            className="speech_bubble"
-                        >
-                            This directive has my most important wishes and my latest data.
-                        </div>
-                    </div>
-                </div>
-
                 <div id="button">
                 <Link to="/create">
                   <NormalButton className="MediumButtonText" >Create my directive</NormalButton>
