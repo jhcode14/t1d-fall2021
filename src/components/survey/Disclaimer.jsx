0e556a312070b55d3a3906d17a2683eb0ae90e8d
jhcode14 <jhsucode@gmail.com>
2022-03-24 00:46:45 -0700

    completed disclaimer

--- src/components/survey/Disclaimer.jsx
+++ src/components/survey/Disclaimer.jsx
@@ -3,6 +3,7 @@ import { Button, Row, Col } from "antd";
 import styles from "./Disclaimer.module.css";
 
 import { Link } from "react-router-dom";
+import { GoInfo } from "react-icons/go";
 
 export default function Disclaimer() {
   return (
@@ -13,22 +14,23 @@ export default function Disclaimer() {
           <div className={styles.containerTitle2}>Privacy Agreement</div>
         </Col>
       </Row>
-      <Row>
+      <Row className={styles.row2}>
         <div className={styles.outTitle}>Prior to the form creation, we ask that you acknowledge our privacy disclaimer. </div>
       </Row>
       <Row>
-        <Col span={12} offset={6} className={styles["shadow-border"]}>
-          <div></div>
+        <Col span={8} offset={8} className={styles["shadow-border"]}>
+          <GoInfo className={styles.icon}/>
           <div className={styles.boxText1}>Privacy Disclaimer</div>
-          <div className={styles.boxText2}>No information will be collected by DDI during this form creation process. All data is stored privately and securely on your browser locally. We value your confidentiality.</div>
-          <Row>
+          <div className={styles.boxText2}>The project is developed by the Diabetes Design Initiative, a part of the Design Lab at University of California, San Diego. We collect no information during this form creation process. All data is stored privately and securely on your browser. We value your privacy.
+</div>
+          <div className={styles.pageControl}>
             <Link to="/">
               <Button className={styles.exitPageButton}>Exit Page</Button>
             </Link>
             <Link to="/create">
               <Button type="primary">Understand and Proceed</Button>
             </Link>
-          </Row>
+          </div>
         </Col>
       </Row>
     </div>
